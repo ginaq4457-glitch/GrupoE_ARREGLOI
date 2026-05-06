205c9c7e8fd133688804f66dab012012325a79dc
Gina Quevedo <ginaq4457@gmail.com>
2026-05-05 23:35:03 -0500

    Add program to invert and display an array

---
+++ Invertir.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() { 
+    int numeros[5] = {1, 2, 3, 4, 5}; 
+Página 15  
+ 
+    cout << "Arreglo invertido:" << endl; 
+ 
+    for(int i = 4; i >= 0; i--) { 
+        cout << numeros[i] << endl; 
+    } 
+ 
+    return 0; 
+} 
